763e4004f124c4176609dac3c001a153c296e0f1
Janet Iqal <j.iqal35@gmail.com>
2021-08-16 14:30:21 -0500

    style.css isnt loading on github,

--- src/page-template.js
+++ src/page-template.js
@@ -37,7 +37,7 @@ function generateTemplate (answers) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-        <link rel="stylesheet" href="/dist/style.css">
+        <link rel="stylesheet" href="./dist/style.css">
 </head>
 
 <body>
